0d1c6a998b8a76312da7e8a72fb730bad4446b80
LoHii <vertti.viitanen@gmail.com>
2022-01-29 14:45:59 +0200

    Add proto for movement and ghosting

---
+++ Assets/Scripts/Mover/Mover.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	[SerializeField] float walkSpeed;
+	[SerializeField] float jumpSpeed;
+	[SerializeField] float gravity;
+	[SerializeField] float maxVSpeed;
+	[SerializeField] float minVSpeed;
+
+	int jumpMask = -1;
+	int groundMask = -1;
+
+	int move = -1;
+	bool jump;
+
+	public Rigidbody2D Rigidbody { get; protected set; }
+
+	public float Vspeed { get; set; }
+
+	private void Awake() {
+		groundMask = LayerMask.GetMask("Solid");
+		jumpMask = LayerMask.GetMask("Solid", "Ghost");
+		Rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	public void SetMoveState(int move, bool jump) {
+		this.move = move;
+		this.jump = jump;
+	}
+
+	void FixedUpdate() {
+		if (!IsGrounded()) Vspeed -= gravity * Time.fixedDeltaTime;
+		else Vspeed = 0;
+		if (jump && CanJump()) Vspeed = jumpSpeed;
+		Vspeed = Mathf.Clamp(minVSpeed, Vspeed, maxVSpeed);
+
+		Rigidbody.MovePosition(Rigidbody.position + (Vector2.up * Vspeed + Vector2.right * move * walkSpeed) * Time.fixedDeltaTime);
+		move = 0;
+		jump = false;
+	}
+
+	private bool IsGrounded() {
+		var groundCheck = Physics2D.Raycast(Rigidbody.position + Vector2.down * transform.lossyScale.y / 2f, Vector2.down, 0.01f, groundMask);
+		return groundCheck.collider != null;
+	}
+
+	private bool CanJump() {
+		var groundCheck = Physics2D.Raycast(Rigidbody.position + Vector2.up * transform.lossyScale.y / 2f, Vector2.down, 0.01f + transform.lossyScale.y, jumpMask);
+		return groundCheck.collider != null;
+	}
+}
---
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] Mover player;
+	[SerializeField] Mover recordPlayer;
+	[SerializeField] Rigidbody2D ghost;
+	[Space]
+	[SerializeField] KeyCode[] left = { KeyCode.LeftArrow, KeyCode.A };
+	[SerializeField] KeyCode[] right = { KeyCode.RightArrow, KeyCode.D };
+	[SerializeField] KeyCode[] jump = { KeyCode.UpArrow, KeyCode.A };
+	[SerializeField] KeyCode[] record = { KeyCode.Space};
+
+	bool recording;
+	bool playing;
+
+	Mover target;
+	List<Vector2> positions = new List<Vector2>();
+	int positionIndex = 0;
+
+	private void Awake() {
+		target = player;
+	}
+
+	void Update() {
+		int press = 0;
+		bool tryJump = GetKeyDown(jump);
+		bool tryRecord = GetKeyDown(record);
+
+		if (GetKey(left)) press -= 1;
+		if(GetKey(right)) press += 1;
+
+		target.SetMoveState(press, tryJump);
+
+		if (tryRecord) {
+			if (playing) StopPlaying();
+			else if (recording) StopRecord();
+			else StartRecord();
+		}
+	}
+
+	private void FixedUpdate() {
+		if (recording) {
+			positions.Add(target.Rigidbody.position);
+		} else if (playing) {
+			ghost.position = positions[positionIndex];
+			positionIndex = (positionIndex + 1) % positions.Count;
+		}
+	}
+
+	private void StartRecord() {
+		positions.Clear();
+		target = recordPlayer;
+		recordPlayer.gameObject.SetActive(true);
+		recordPlayer.Rigidbody.position = player.Rigidbody.position;
+		recordPlayer.Vspeed = player.Vspeed;
+		recording = true;
+	}
+
+	private void StopRecord() {
+		positionIndex = 0;
+		ghost.gameObject.SetActive(true);
+		recordPlayer.gameObject.SetActive(false);
+		target = player;
+		recording = false;
+		playing = true;
+	}
+
+	private void StopPlaying() {
+		ghost.gameObject.SetActive(false);
+		playing = false;
+	}
+
+	private bool GetKey(params KeyCode[] keys) {
+		foreach(KeyCode key in keys) {
+			if (Input.GetKey(key))
+				return true;
+		}
+		return false;
+	}
+
+	private bool GetKeyDown(params KeyCode[] keys) {
+		foreach (KeyCode key in keys) {
+			if (Input.GetKeyDown(key))
+				return true;
+		}
+		return false;
+	}
+}
